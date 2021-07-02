9c162e76f833580d715a807789ef831e9953904d
JIMA <JIMA@MSI>
2021-07-02 23:26:23 +0800

    Login Bean Update

--- src/main/java/com/petpet/Member/LoginBean.java
+++ src/main/java/com/petpet/Member/LoginBean.java
@@ -1,33 +1,109 @@
 package com.petpet.Member;
 
-public class LoginBean implements java.io.Serializable {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "member")
+public class LoginBean {
+
 	
-	private static final long serialVersionUID = 1L;
+	@Id @Column(name = "MEMBERID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private String memberid;
+
+	@Column(name = "EMAIL")
 	private String email;
+
+	@Column(name = "PASSWORD")
 	private String password;
+	
+	@Column(name = "FIRSTNAME")
 	private String firstname;
+	
+	@Column(name = "LASTNAME")
 	private String lastname;
+	
+	@Column(name = "GENDER")
 	private String gender;
+	
+	@Column(name = "BIRTHDAY")
 	private String birthday;
+	
+	@Column(name = "MOBILE")
 	private String mobile;
-	
-	public String getMemberid() {  return memberid;  }
-	public String getEmail() {  return email;  }
-	public String getPassword() {  return password;  }
-	public String getFirstname() {  return firstname;  }
-	public String getLastname() {  return lastname;  }
-	public String getGender() {  return gender;  }
-	public String getBirthday() {  return birthday;  }
-	public String getMobile() {  return mobile;  }
-	
-	public void setMemberid(String memberid) {  this.memberid = memberid;  }
-	public void setEmail(String email) {  this.email = email;  }
-	public void setPassword(String password) {  this.password = password;  }
-	public void setFirstname(String firstname) {  this.firstname= firstname;  }
-	public void setLastname(String lastname) {  this.lastname = lastname;  }
-	public void setGender(String gender) {  this.gender= gender;	}
-	public void setBirthday(String birthday) {  this.birthday= birthday;	}
-	public void setMobile(String mobile) {  this.mobile= mobile;	}
-	
+
+	public LoginBean() {
+
+	}
+
+	public String getMemberid() {
+		return memberid;
+	}
+
+	public void setMemberid(String memberid) {
+		this.memberid = memberid;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+}
 }
