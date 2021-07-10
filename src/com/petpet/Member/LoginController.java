220c01a7ee3128716fd0081d815ff630b2de4d62
JIMA <JIMA@MSI>
2021-07-10 15:31:20 +0800

    test

--- src/com/petpet/Member/LoginController.java
+++ src/com/petpet/Member/LoginController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,8 +41,6 @@ public class LoginController {
 
 		errors.put("message", "please input correct username and password");
 		return "Login";
-		
-		
 	}
 
 }
